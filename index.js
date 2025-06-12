c0782136c424bc6b33acfb175dc3a615f2286e16
Itachi <itachijodx72@gmail.com>
2025-06-12 13:01:58 +0545

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function fetchDataWithRetry(username, retries = 2) {
 app.get('/', (req, res) => {
   res.send('tiktok');
 });
-app.get('/kshitiz', async (req, res) => {
+app.get('/itachi', async (req, res) => {
   try {
     const username = req.query.username;
     if (!username) {
